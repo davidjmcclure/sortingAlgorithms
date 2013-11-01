b564a78799fd4155081f949fd7b0dfd9745ac727
David McClure <david180591@gmail.com>
2013-11-01 17:26:09 +0000

    merge sort implementation complete

--- src/algorithms/mergesort/Mergesort.java
+++ src/algorithms/mergesort/Mergesort.java
@@ -1,6 +1,7 @@
 package algorithms.mergesort;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import algorithms.IAlgorithm;
 
@@ -8,13 +9,55 @@ public class Mergesort implements IAlgorithm{
 
 	@Override
 	public ArrayList<Integer> sortList(ArrayList<Integer> unsortedList) {
-		
+		ArrayList<Integer> sorted = SplitMerge(unsortedList, 0, unsortedList.size() - 1);
+		return sorted;
 	}
 
 	private ArrayList<Integer> SplitMerge(ArrayList<Integer> unsorted, int start, int end){
-		if((end-start+1)/2 < 2){
+		if((end-start) == 0){
+			ArrayList<Integer> sorted = new ArrayList<Integer>();
+			sorted.add(unsorted.get(start));
 			
+			return sorted;
 		}
+		
+		int mid = (end-start)/2 + start;
+
+		ArrayList<Integer> partialA = SplitMerge(unsorted, start, mid);
+		ArrayList<Integer> partialB = SplitMerge(unsorted, mid+1, end);
+		
+		int ACheck = 0;
+		int BCheck = 0;
+		
+		ArrayList<Integer> output = new ArrayList<Integer>();
+
+		while(ACheck < partialA.size() && BCheck < partialB.size()){
+			if(partialA.get(ACheck) < partialB.get(BCheck)){
+				output.add(partialA.get(ACheck));
+				ACheck += 1;
+			} else if(partialA.get(ACheck) > partialB.get(BCheck)){
+				output.add(partialB.get(BCheck));
+				BCheck += 1;
+			} else if(partialA.get(ACheck) == partialB.get(BCheck)){
+				output.add(partialB.get(BCheck));
+				output.add(partialA.get(ACheck));
+				ACheck += 1;
+				BCheck += 1;
+			}
+		}
+
+
+		if(ACheck == partialA.size() & BCheck < partialB.size()){
+			for(; BCheck < partialB.size(); BCheck++){
+				output.add(partialB.get(BCheck));
+			}
+		}else if(BCheck == partialB.size() & ACheck < partialA.size()){
+			for(; ACheck < partialA.size(); ACheck++){
+				output.add(partialA.get(ACheck));
+			}	
+		}
+
+		return output;
 	}
 	
 	@Override
--- src/main/TestSet.java
+++ src/main/TestSet.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 import algorithms.insertionsort.Insertionsort;
+import algorithms.mergesort.Mergesort;
 
 public class TestSet {
 
 	@Test
-	public void testSortList() {
+	public void testInsertionSortList() {
 		
 		ArrayList<Integer> sorted = new ArrayList<Integer>();
 		ArrayList<Integer> unsorted = new ArrayList<Integer>();
@@ -32,6 +33,33 @@ public class TestSet {
 		unsorted.add(5);
 		
 		Insertionsort testAlgorithm = new Insertionsort();
+		
+		assertEquals("List should be sorted", sorted, testAlgorithm.sortList(unsorted));
+	}
+	
+	@Test
+	public void testMergeSortList() {
+		
+		ArrayList<Integer> sorted = new ArrayList<Integer>();
+		ArrayList<Integer> unsorted = new ArrayList<Integer>();
+				
+		sorted.add(1);
+		sorted.add(2);
+		sorted.add(3);
+		sorted.add(4);
+		sorted.add(5);
+		sorted.add(6);
+		sorted.add(7);
+		
+		unsorted.add(2);
+		unsorted.add(4);
+		unsorted.add(7);
+		unsorted.add(6);
+		unsorted.add(3);
+		unsorted.add(1);
+		unsorted.add(5);
+		
+		Mergesort testAlgorithm = new Mergesort();
 		
 		assertEquals("List should be sorted", sorted, testAlgorithm.sortList(unsorted));
 	}
