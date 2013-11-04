d988ae23772d61ac9e8aad7ff4b728af832c4a00
David McClure <david180591@gmail.com>
2013-11-04 22:39:33 +0000

    bubblesort implemented

--- src/main/TestSet.java
+++ src/main/TestSet.java
@@ -1,10 +1,13 @@
 package main;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import gui.GUIControls;
+
 import java.util.ArrayList;
+
 import org.junit.Test;
 
+import algorithms.bubblesort.Bubblesort;
 import algorithms.insertionsort.Insertionsort;
 import algorithms.mergesort.Mergesort;
 
@@ -60,6 +63,33 @@ public class TestSet {
 		unsorted.add(5);
 		
 		Mergesort testAlgorithm = new Mergesort();
+		
+		assertEquals("List should be sorted", sorted, testAlgorithm.sortList(unsorted));
+	}
+	
+	@Test
+	public void testBubbleSortList() {
+		
+		ArrayList<Integer> sorted = new ArrayList<Integer>();
+		ArrayList<Integer> unsorted = new ArrayList<Integer>();
+				
+		sorted.add(1);
+		sorted.add(2);
+		sorted.add(3);
+		sorted.add(4);
+		sorted.add(5);
+		sorted.add(6);
+		sorted.add(7);
+		
+		unsorted.add(2);
+		unsorted.add(4);
+		unsorted.add(7);
+		unsorted.add(6);
+		unsorted.add(3);
+		unsorted.add(1);
+		unsorted.add(5);
+		
+		Bubblesort testAlgorithm = new Bubblesort();
 		
 		assertEquals("List should be sorted", sorted, testAlgorithm.sortList(unsorted));
 	}
