09f668f7abda5a49f4e639c9f941df45a09e927c
David McClure <david180591@gmail.com>
2013-11-06 23:32:53 +0000

    Began implementing Quicksort

---
+++ src/algorithms/quicksort/Quicksort.java
@@ -0,0 +1,21 @@
+package algorithms.quicksort;
+
+import java.util.ArrayList;
+
+import algorithms.IAlgorithm;
+
+public class Quicksort implements IAlgorithm {
+
+	@Override
+	public ArrayList<Integer> sortList(ArrayList<Integer> unsortedList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String returnCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
