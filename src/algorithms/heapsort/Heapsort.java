eab9ccb32793c8395f075a61f3a9c3dd7018b033
David McClure <david180591@gmail.com>
2013-11-07 23:25:19 +0000

    updated heapsort, added sift function

--- src/algorithms/heapsort/Heapsort.java
+++ src/algorithms/heapsort/Heapsort.java
@@ -1,6 +1,7 @@
 package algorithms.heapsort;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import algorithms.IAlgorithm;
 
@@ -17,6 +18,31 @@ public class Heapsort implements IAlgorithm {
 		
 	}
 
+	private ArrayList<Integer> siftDown(ArrayList<Integer> A, int start, int end){
+		
+		int root = start;
+		int child, swap;		
+		
+		while((root*2) + 1 <= end){
+			child = (root*2)+1;
+			swap = root;
+			if(A.get(swap) < A.get(child)){
+				swap = child;
+			}
+			if(child+1 <= end & A.get(swap) < A.get(child+1)){
+				swap = child + 1;
+			}
+			if(swap != root){
+				Collections.swap(A, root, swap);
+				root = swap;
+			}
+			else{
+				return A;
+			}
+		}
+		return A;		
+	}
+	
 	@Override
 	public String returnCode() {
 		// TODO Auto-generated method stub
